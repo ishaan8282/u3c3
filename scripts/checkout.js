b59bb8135c6e3bd4e8c97b97355b5afa2062c1ea
ishaan8282 <mehtaishan8282@gmail.com>
2022-04-25 12:34:36 +0530

    eval

---
+++ scripts/checkout.js
@@ -0,0 +1,38 @@
+// // Each ticket will cost 100 Rupees
+// // If wallet amount is insufficient show alert "Insufficient Balance!";
+// // Else show "Booking successfull!" and adjust the wallet amount in real time
+// let moviedata =   JSON.parse(localStorage.getItem("movies"))
+// console.log(moviedata)
+
+// moviedata.map(function(el){
+
+
+//   let div1 = document.createElement("div")
+//   div1.setAttribute("id","div")
+//   let image = document.createElement("img")
+//    image.src = el.Poster
+
+//    let title = document.createElement("h3")
+//    title.innerText = el.Title
+
+//    div1.append(image,title)
+//    movie.append(div1)
+
+// })
+
+// let walletamount = JSON.parse(localStorage.getItem("amount"))
+// console.log(walletamount)
+
+// function confirm(){
+
+//   // alert("you clik on confirm")
+
+//  if(walletamount<100)
+//  {
+//    alert("Insufficient Balance")
+//  }
+//  else
+//  {
+//    alert("Booking successful!")
+//  }
+// }
